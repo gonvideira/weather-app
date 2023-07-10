aa7663190789bbb4f15fc80080503db2ca73d79d
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-10 11:52:43 +0100

    Update test-python.py

--- test-python.py
+++ test-python.py
@@ -5,6 +5,7 @@ try:
     API_KEY = os.environ['WEATHER_MAP_API']
 except KeyError:
     API_KEY = 'Token not available!'
+    print(API_KEY)
     # or raise an error if it's not available so that the workflow fails
 
 response = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=API_KEY')
