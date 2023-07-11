81c81a5a328b3f959c7e3e808e765b89d5ed9332
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-11 08:47:19 +0100

    Update test-python.py

--- test-python.py
+++ test-python.py
@@ -1,4 +1,5 @@
 import requests
+import certifi
 import os
 
 try:
@@ -8,8 +9,21 @@ except KeyError:
     print(API_KEY)
     # or raise an error if it's not available so that the workflow fails
 
+try:
+  print('Checking connection to Github...')
+  test = requests.get('https://api.github.com')
+  print('Connection to Github OK.')
+except requests.exceptions.SSLError as err:
+  print('SSL Error. Adding custom certs to Certifi store...')
+  cafile = certifi.where()
+  with open('certicate.pem', 'rb') as infile:
+    customca = infile.read()
+  with open(cafile, 'ab') as outfile:
+    outfile.write(customca)
+    print('That might have worked.')
+
 # response = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid='+API_KEY+'&lang=pt')
-response = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=' + API_KEY)
+# response = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=' + API_KEY)
 # response = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99')
 # test in mobile phone
 
