838f2815096d6df2f5e2b98b643568e1a3b05a68
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-11 08:48:34 +0100

    Update test-python.py

--- test-python.py
+++ test-python.py
@@ -11,7 +11,7 @@ except KeyError:
 
 try:
   print('Checking connection to Github...')
-  test = requests.get('https://api.github.com')
+  response = requests.get('https://api.github.com')
   print('Connection to Github OK.')
 except requests.exceptions.SSLError as err:
   print('SSL Error. Adding custom certs to Certifi store...')
