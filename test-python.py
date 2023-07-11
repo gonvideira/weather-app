8907d2b548306479a46444f90faec4dd9757530e
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-11 14:32:32 +0100

    Update test-python.py

--- test-python.py
+++ test-python.py
@@ -9,19 +9,6 @@ except KeyError:
     print(API_KEY)
     # or raise an error if it's not available so that the workflow fails
 
-try:
-  print('Checking connection to Github...')
-  response_Git = requests.get('https://api.github.com')
-  print('Connection to Github OK.')
-except requests.exceptions.SSLError as err:
-  print('SSL Error. Adding custom certs to Certifi store...')
-  cafile = certifi.where()
-  with open('certicate.pem', 'rb') as infile:
-    customca = infile.read()
-  with open(cafile, 'ab') as outfile:
-    outfile.write(customca)
-    print('That might have worked.')
-
 # url = 'http://api.openweathermap.org/data/2.5/weather?q=Caparica,pt&APPID=' + API_KEY
 lat = 38.60492907958181
 lon = -9.211457576882433
