d746ba13a12e6a477a0a97a34651fe13a3973fa0
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-28 10:43:36 +0100

    included arrow html code to test

--- treat_weather_data.py
+++ treat_weather_data.py
@@ -11,34 +11,7 @@ import pytz
 FN = 'output/weather-output.json'
 OUTPUT = 'output/README.md'
 TITLE = '⛅ ISLANTILLA!'
-ARROW = '<table>
-        <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-        </tr>
-        <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>
-                <svg viewBox="0 0 350 100">
-                    <defs>
-                    <marker id="arrowhead" markerWidth="10" markerHeight="7" 
-                        refX="0" refY="3.5" orient="auto">
-                        <polygon points="0 0, 10 3.5, 0 7" />
-                    </marker>
-                    </defs>
-                    <line x1="0" y1="50" x2="250" y2="50" stroke="#000" 
-                        stroke-width="8" marker-end="url(#arrowhead)" />
-                </svg>
-            </td>
-            <td>Mexico</td>
-        </tr>
-    </table>'
+ARROW = '<table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td><svg viewBox="0 0 350 100"><defs><marker id="arrowhead" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto"><polygon points="0 0, 10 3.5, 0 7" /></marker></defs><line x1="0" y1="50" x2="250" y2="50" stroke="#000" stroke-width="8" marker-end="url(#arrowhead)" /></svg></td><td>Mexico</td></tr></table>'
 
 def forecast_date():
     """Function to get forecast date"""
@@ -78,6 +51,8 @@ class ConvertJson():
         """Function to transform json data to md"""
         text = f'# {self.h1_heading}\n'
         text += f'```Forecast date {date_now}```\n\n'
+        text += ARROW
+        text += '\n\n'
         data_list = self.jdata
         for dct in data_list:
             localized_date = localize_date(dct["dt_txt"])
